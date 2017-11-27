65b696a03b6f4b10007f0e6697772c6829b9b3e3
Timothy Pharo <Tim.Pharo@autotrader.co.uk>
2017-11-27 20:52:39 +0000

    Introduce application

---
+++ src/main/kotlin/uk/co/autotrader/advertbackend/Application.kt
@@ -0,0 +1,11 @@
+package uk.co.autotrader.advertbackend
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Application::class.java, *args)
+}
