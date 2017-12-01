68a4cf7769ec9aa16955cf8d9c354a7d23397b02
Timothy Pharo <Tim.Pharo@autotrader.co.uk>
2017-12-01 23:25:24 +0000

    Add required Jedis deps

--- src/main/kotlin/com/pharo/alexa/Application.kt
+++ src/main/kotlin/com/pharo/alexa/Application.kt
@@ -7,6 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 class Application
 
 fun main(args: Array<String>) {
-    println("Redis URL: ${System.getenv("REDIS_URL")}")
     SpringApplication.run(Application::class.java, *args)
 }
